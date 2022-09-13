e573a33c15d99ded5ef856758b9a660f817f7494
Mary Karakida <makarakida@gmail.com>
2022-09-13 13:41:20 -0300

    feat: create auth router

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,16 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { json } from 'express';
 
+import router from './routes/router';
+
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(json());
+
+app.use(router);
 
 const PORT: number = Number(process.env.PORT) || 4000;
 
