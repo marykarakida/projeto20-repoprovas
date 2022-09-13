270055eb42a935e2844fc9a9af27f16a01d7d2ae
Mary Karakida <makarakida@gmail.com>
2022-09-12 22:43:36 -0300

    chore: set up eslint and prettier

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import cors from "cors";
-import dotenv from "dotenv";
-import express, { json } from "express";
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express, { json } from 'express';
 
 dotenv.config();
 
@@ -9,9 +9,9 @@ const app = express();
 app.use(cors());
 app.use(json());
 
-const PORT : number = Number(process.env.PORT) || 4000;
+const PORT: number = Number(process.env.PORT) || 4000;
 
 app.listen(PORT, () => {
-    console.log("Server running on port", PORT)
-})
-
+    // eslint-disable-next-line no-console
+    console.log('Server running on port', PORT);
+});
