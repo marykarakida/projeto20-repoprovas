09b2794a7602a3922f8bb0deb5961e0b57b28f69
Mary Karakida <makarakida@gmail.com>
2022-09-13 19:31:13 -0300

    chore: create jwt utils

--- environment.d.ts
+++ environment.d.ts
@@ -4,6 +4,10 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             PORT?: string;
+            DATABASE_URL: string;
+            SHADOW_DATABASE_URL?: string;
+            TOKEN_SECRET: string;
+            JWT_EXPIRE: string;
         }
     }
 }
